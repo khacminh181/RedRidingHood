49a3feb561a07b185ff6e3640540e0cf88d60a74
Minh Nguyen <nguyenkhacminh181@gmail.com>
2017-11-16 20:14:11 +0700

    Added tut

--- src/Entity/Platform/Platform.java
+++ src/Entity/Platform/Platform.java
@@ -11,6 +11,7 @@ public class Platform extends GameObject implements PhysicsBody{
     public static final int BLOCKTILE = 1;
     public static final int HORNTILE = 2;
     public static final int DOORTILE = 3;
+    public static final int KEYTYPE = 4;
 
 
     public int isType;
@@ -36,7 +37,9 @@ public class Platform extends GameObject implements PhysicsBody{
         super();
         //this.renderer = new ImageRenderer("assets/images/yellow_square.jpg");
         this.boxCollider = new BoxCollider(32, 32);
-        this.children.add(boxCollider);
+//        if (isType != KEYTYPE) {
+//            this.children.add(boxCollider);
+//        }
     }
 
 
@@ -48,24 +51,40 @@ public class Platform extends GameObject implements PhysicsBody{
     public static Platform create(int platformType) {
         Platform platform = new Platform();
         switch (platformType) {
+            case 7:
+                platform.renderer = new ImageRenderer(SpriteUtils.loadImage("assets/images/Map1/up.png"));
+                platform.isType = KEYTYPE;
+                break;
+            case 6:
+                platform.renderer = new ImageRenderer(SpriteUtils.loadImage("assets/images/Map1/C.png"));
+                platform.isType = KEYTYPE;
+                break;
+            case 5:
+                platform.renderer = new ImageRenderer(SpriteUtils.loadImage("assets/images/Map1/X.png"));
+                platform.isType = KEYTYPE;
+                break;
             case 4:
                 platform.renderer = new ImageRenderer(SpriteUtils.loadImage("assets/images/Map1/door.png"));
                 platform.isType = DOORTILE;
+                platform.children.add(platform.boxCollider);
                 break;
             case 3:
                 platform.renderer = new ImageRenderer(SpriteUtils.loadImage("assets/images/Map1/Horn.png"));
-               // platform.children.add(platform.boxCollider);
                 platform.isType = HORNTILE;
+                platform.children.add(platform.boxCollider);
+
                 break;
             case 2:
                 platform.renderer = new ImageRenderer(SpriteUtils.loadImage("assets/images/Map1/grass.png"));
-                //platform.children.add(platform.boxCollider);
                 platform.isType = NORMALTILE;
+                platform.children.add(platform.boxCollider);
+
                 break;
             case 1:
                 platform.renderer = new ImageRenderer(SpriteUtils.loadImage("assets/images/Map1/dirt.png"));
-                //platform.children.add(platform.boxCollider);
                 platform.isType = BLOCKTILE;
+                platform.children.add(platform.boxCollider);
+
                 break;
         }
 
